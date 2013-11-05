f33ff60a98d6971d01e3a189eda5fce6af24a580
Michelle Torres <lic.nancy.torres@gmail.com>
2013-11-05 12:23:59 -0600

    Ejercicio del futbol

---
+++ Estructuras/registroequipo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct a{
+	string equipo;
+	string director;
+	int p_jugados, p_ganados, p_perdidos;
+	int goles_favor, goles_contra;
+	bool tiene_porra;
+};
+
+typedef a registro_equipo;
+
+
+int main(){
+	registro_equipo grupoA[6], grupoB[4];
+
+	for(int i=0; i<6; i++){
+		cout << "Datos para el equipo " << i+1 << endl;
+
+		cout << "Dame el nombre del equipo: ";
+		cin >> grupoA[i].equipo;
+		cout << "Cuantos partidos gano: ";
+		cin >> grupoA[i].p_ganados;
+	}
+}
